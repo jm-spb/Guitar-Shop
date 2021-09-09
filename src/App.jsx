5fca899fdd928f2daad90c203ceb591986ff007f
ivan <tuchel.ivan@gmail.com>
2021-09-09 20:54:14 +0300

    adjust index-page styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,78 +1,40 @@
 import React from 'react';
-import './scss/App.scss';
 
-import logo from './assets/icons/GS logo.png';
+import './App.scss';
+import './components/Cart/Cart.scss';
+
 import edwards from './assets/img/guitar/min/EdwardsLP.webp';
 import epiphone from './assets/img/guitar/min/epiphone.webp';
 import fender from './assets/img/guitar/min/fender.webp';
 import gibson from './assets/img/guitar/min/gibson.webp';
 import jackson from './assets/img/guitar/min/jackson.webp';
 import solar from './assets/img/guitar/min/solar.webp';
-import cart from './assets/icons/cart.png';
-import triangle from './assets/icons/triangle.png';
 
-function App() {
+import { Header, SelectBar } from './components';
+import { Cart } from './components/Cart/Cart';
+
+export const App = () => {
 	return (
 		<div className="index-page">
 			<div className="index-page__content container">
-				{/* HEADER */}
 				<div className="index-page__header">
-					<header className="header">
-						<div className="header__container">
-							<div className="header__logo">
-								<img src={logo} width={40} height={40} alt="logo" />
-								<div className="header__title">
-									<h1>GUITAR SHOP</h1>
-									<p>Продажа гитар и музыкального оборудования</p>
-								</div>
-							</div>
-							<div className="header__cart">
-								<a href="/cart.html" className="button button-cart">
-									<span>10000 ₽</span>
-									<div className="button__delimiter" />
-									<img src={cart} alt="cart" />
-									<span>3</span>
-								</a>
-							</div>
-						</div>
-					</header>
+					<Header />
 				</div>
+
+				{/* <Cart /> */}
 
 				{/* SELECT BAR */}
 				<div className="index-page__select-bar">
-					<div className="select-bar">
-						<div className="select-bar__categories">
-							<ul>
-								<li className="select-bar__categories_active">Все</li>
-								<li>Гитары</li>
-								<li>Бас-гитары</li>
-								<li>Усилители</li>
-								<li>Кабинеты</li>
-								<li>Микрофоны</li>
-							</ul>
-						</div>
-						<div className="select-bar__sort sort">
-							<div className="sort__label">
-								<img src={triangle} width={10} height={10} alt="triangle" />
-								<b>Сортировка по:</b>
-								<span>популярности</span>
-							</div>
-							<div className="sort__popup">
-								<ul>
-									<li className="sort__popup_active">популярности</li>
-									<li>цене</li>
-									<li>алфавиту</li>
-								</ul>
-							</div>
-						</div>
-					</div>
+					<SelectBar
+						onClickItem={(name) => alert(name)}
+						items={[ 'Все', 'Гитары', 'Бас-гитары', 'Усилители', 'Кабинеты', 'Микрофоны' ]}
+					/>
 				</div>
 
 				{/* MAIN */}
 				<main className="index-page__main main">
 					<h2>Все товары</h2>
 					<div className="main__content">
-						{/*  */}
 						<div className="main__item-card item-card">
 							<img className="item-card__image" width={100} height={250} src={edwards} alt="Edwards" />
 							<h4 className="item-card__title">Edwards</h4>
@@ -102,7 +64,6 @@ function App() {
 								</div>
 							</div>
 						</div>
-						{/*  */}
 						<div className="main__item-card item-card">
 							<img className="item-card__image" width={100} height={250} src={epiphone} alt="Epiphone" />
 							<h4 className="item-card__title">Epiphone</h4>
@@ -131,7 +92,7 @@ function App() {
 									<i>2</i>
 								</div>
 							</div>
-						</div>{' '}
+						</div>
 						<div className="main__item-card item-card">
 							<img className="item-card__image" width={100} height={250} src={fender} alt="Fender" />
 							<h4 className="item-card__title">Fender</h4>
@@ -160,7 +121,7 @@ function App() {
 									<i>2</i>
 								</div>
 							</div>
-						</div>{' '}
+						</div>
 						<div className="main__item-card item-card">
 							<img className="item-card__image" width={100} height={250} src={gibson} alt="Gibson" />
 							<h4 className="item-card__title">Gibson</h4>
@@ -189,7 +150,7 @@ function App() {
 									<i>2</i>
 								</div>
 							</div>
-						</div>{' '}
+						</div>
 						<div className="main__item-card item-card">
 							<img className="item-card__image" width={100} height={250} src={jackson} alt="Jackson" />
 							<h4 className="item-card__title">Jackson</h4>
@@ -218,7 +179,7 @@ function App() {
 									<i>2</i>
 								</div>
 							</div>
-						</div>{' '}
+						</div>
 						<div className="main__item-card item-card">
 							<img className="item-card__image" width={100} height={250} src={solar} alt="Solar" />
 							<h4 className="item-card__title">Solar</h4>
@@ -247,12 +208,10 @@ function App() {
 									<i>2</i>
 								</div>
 							</div>
-						</div>{' '}
+						</div>
 					</div>
 				</main>
 			</div>
 		</div>
 	);
-}
-
-export default App;
+};
