56c323f9023db57ef275f0cf58459c2eaa98be7a
ivan <tuchel.ivan@gmail.com>
2021-09-13 13:44:05 +0300

    add ItemCard

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { Header } from './Header/Header';
 export { SelectBar } from './SelectBar/SelectBar';
 export { Cart } from './Cart/Cart';
 export { EmptyCart } from './EmptyCart/EmptyCart';
+export { ItemCard } from './ItemCard/ItemCard';
