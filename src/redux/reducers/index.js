c8266039212cacb73ec000d6f87bc22ee7b5233b
ivan <tuchel.ivan@gmail.com>
2021-09-30 15:38:29 +0300

    add redux-persist

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,11 +1,21 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import session from 'redux-persist/lib/storage/session';
 
 import { cart } from './cart';
 import { filters } from './filters';
 import { itemCards } from './itemCards';
 
-export const rootReducer = combineReducers({
+const persistConfig = {
+  key: 'root',
+  storage: session,
+  whitelist: [ 'cart' ]
+};
+
+const rootReducer = combineReducers({
   filters,
   itemCards,
   cart
 });
+
+export default persistReducer(persistConfig, rootReducer);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,13 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { persistStore } from 'redux-persist';
 
-import { rootReducer } from './reducers';
+import persistReducer from './reducers';
 
 // composeEnhancers - для корректной работы ReduxDevTools и функций посредников (middleware)
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 // applyMiddleware - взять все функции посредники, и использовать их при каждом action
-export const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+export const store = createStore(persistReducer, composeEnhancers(applyMiddleware(thunk)));
+
+export const persistor = persistStore(store);
