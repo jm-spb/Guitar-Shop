cc1e41c701d379cf6fd5689c7afa415fe13a3260
ivan <tuchel.ivan@gmail.com>
2023-02-10 19:42:34 +0300

    set project to front only

--- src/api/fetchItems.js
+++ src/api/fetchItems.js
@@ -2,8 +2,13 @@ import axios from 'axios';
 
 import { setLoaded, setItemCards, setErrorMsg } from '../redux/actions/itemCards';
 
+const api = axios.create({
+  withCredentials: true,
+  baseURL: 'https://guitart-shop-backend.onrender.com',
+});
+
 const fetchItems = (category, sortBy, order) => (dispatch) => {
-  axios
+  api
     .get(`/${category}?_sort=${sortBy}&_order=${order}`)
     .finally(dispatch(setLoaded(false)))
     .then(({ data }) => {
